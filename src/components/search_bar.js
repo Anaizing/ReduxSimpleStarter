0bc479459e58b4db994e6c27b395ed409e9f91e1
Barry Closser <barry.closser@gmail.com>
2017-09-14 14:22:59 -0500

    command line commit

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,22 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 class SearchBar extends Component {
+  // State is a plain JS object that is used to record and react to user events
+  constructor(props) {
+    super(props)
+
+    this.state = { term: 'starting value' }
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />
+    return (
+      <div>
+        <input
+          value = {this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+      </div>
+    )
   }
 }
 
